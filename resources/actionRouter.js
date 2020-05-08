26a601b5df655abbac2335eef29a2487744d077c
Victoria  Topham <topham.victoria@gmail.com>
2020-05-08 08:23:58 -0700

    laid out all endpoints and middleware that i need to create

--- resources/actionRouter.js
+++ resources/actionRouter.js
@@ -1,6 +1,43 @@
+//actions
 const express = require('express')
 const aModel = require('../data/helpers/actionModel.js')
 
 const router = express.Router();
+
+//get a list of all actions
+router.get('/', (req, res) => {
+    
+})
+
+//create an action
+router.post('/', (req,res) => {
+
+})
+
+//get action by id
+router.get('/:id', (req, res) => {
+    
+})
+
+//delete an action by id
+router.delete('/:id', (req, res) => {
+    
+})
+
+//edit an action by id
+router.put('/:id', (req, res) => {
+
+})
+
+//custom middleware
+
+function validateActionID(req, res, next) {
+
+}
+
+function validateAction(req, res, next) {
+    
+}
+
 
 module.exports = router;
--- resources/projectRouter.js
+++ resources/projectRouter.js
@@ -1,10 +1,49 @@
+//projects 
 const express = require('express')
 const pModel = require('../data/helpers/projectModel')
 
 const router = express.Router();
 
+//get a list of all projects
 router.get('/', (req, res) => {
-    res.status(200).json({message: "You'r reached projects"})
+    
 })
+
+//create a new project
+router.post('/', (req, res) => {
+
+})
+
+//get projects by id
+router.get('/:id', (req, res) => {
+
+})
+
+//delete a project by id
+router.delete('/:id', (req, res) => {
+    
+})
+
+//edit a post by id
+router.put('/:id', (req, res) => {
+    
+})
+
+//get actions by project id
+router.get('/:id/actions', (req, res) => {
+    
+})
+
+//custom middleware
+
+function validateProjectId(req, res, next) {
+
+}
+
+function validateProject(req, res, next) {
+
+}
+
+
 
 module.exports = router;
